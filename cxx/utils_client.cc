e760bcbc6afda3d8ef91a314c434928455463f26
evenpengqnap <shinycockorach@gmail.com>
2018-11-06 15:33:35 +0800

    Implement Sum

--- cxx/utils_client.cc
+++ cxx/utils_client.cc
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <memory>
 #include <string>
-#include <thread>
+#include <vector>
+#include <random>
 
 #include <grpc/grpc.h>
 #include <grpc++/grpc++.h>
@@ -21,26 +22,78 @@ using utils::DoubleArray;
 using utils::UtilsServer;
 
 class UtilsServerClient {
- public:
-  UtilsServerClient(std::shared_ptr<Channel> channel): stub_(UtilsServer::NewStub(channel)) {}
+  public:
+    UtilsServerClient(std::shared_ptr<Channel> channel)
+        : stub_(UtilsServer::NewStub(channel)) {}
 
-  void Sort() {
-    ClientContext context;
+    double Sum(std::vector<double> &array) {
+        ClientContext context;
 
-    // if (!status.ok()) {
-    //   std::cout << status.error_code() << ": " << status.error_message()
-    //             << std::endl;
-    // }
-  }
+        DoubleArray request;
+        Double response;
 
- private:
-  std::unique_ptr<UtilsServer::Stub> stub_;
+        for (int i = 0; i < array.size(); i++) {
+            request.add_array(array[i]);
+        }
+
+        Status status = stub_->Sum(&context, request, &response);
+
+        if (!status.ok()) {
+            std::cout << status.error_code() << ": " << status.error_message()
+                      << std::endl;
+        }
+
+        return response.value();
+    }
+
+    void Sort() {
+        ClientContext context;
+
+        // if (!status.ok()) {
+        //   std::cout << status.error_code() << ": " << status.error_message()
+        //             << std::endl;
+        // }
+    }
+
+  private:
+    std::unique_ptr<UtilsServer::Stub> stub_;
 };
 
-int main(int argc, char** argv) {
-  UtilsServerClient server(grpc::CreateChannel("localhost:6666", grpc::InsecureChannelCredentials()));
+bool test_Sum(int len) {
+    std::vector<double> array(len);
+    double ans = 0;
 
-  server.Sort();
+    std::default_random_engine generator;
+    std::uniform_real_distribution<double> distribution(0.0, 1.0);
 
-  return 0;
+    for (int i = 0; i < len; i++) {
+        array[i] = distribution(generator);
+        ans += array[i];
+    }
+
+    UtilsServerClient server(grpc::CreateChannel(
+        "localhost:6666", grpc::InsecureChannelCredentials()));
+
+    double grpc_ans = server.Sum(array);
+
+    if (ans != grpc_ans) {
+        std::cout << "[FAILED] test_sum" << std::endl;
+        std::cout << "[FAILED] ans = " << ans << ", grpc_ans = " << grpc_ans
+                  << std::endl;
+        return false;
+    } else {
+        std::cout << "[PASSED] test_sum, ans = " << ans << std::endl;
+        return true;
+    }
+}
+
+void test_all() { test_Sum(10); }
+
+int main(int argc, char **argv) {
+    // UtilsServerClient server(grpc::CreateChannel(
+    //     "localhost:6666", grpc::InsecureChannelCredentials()));
+
+    test_all();
+
+    return 0;
 }
--- cxx/utils_server.cc
+++ cxx/utils_server.cc
@@ -16,33 +16,49 @@ using utils::UtilsServer;
 
 // Logic and data behind the server's behavior.
 class UtilsServerImpl final : public UtilsServer::Service {
-	Status Sort(ServerContext* context, const DoubleArray* request, DoubleArray* response) override {
-		
-		return Status::OK;
-	}
+    Status Sum(ServerContext *context, const DoubleArray *request,
+               Double *response) override {
+        std::cout << "[Sum] Get request" << std::endl;
+        double sum = 0.0;
+
+        for (int i = 0; i < request->array_size(); i++) {
+            sum += request->array(i);
+        }
+
+        response->set_value(sum);
+        std::cout << "[Sum] Done, sum = " << sum << std::endl;
+
+        return Status::OK;
+    }
+
+    Status Sort(ServerContext *context, const DoubleArray *request,
+                DoubleArray *response) override {
+
+        return Status::OK;
+    }
 };
 
 void RunServer() {
-  std::string server_address("0.0.0.0:6666");
-  UtilsServerImpl service;
+    std::string server_address("0.0.0.0:6666");
+    UtilsServerImpl service;
 
-  ServerBuilder builder;
-  // Listen on the given address without any authentication mechanism.
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  // Register "service" as the instance through which we'll communicate with
-  // clients. In this case it corresponds to an *synchronous* service.
-  builder.RegisterService(&service);
-  // Finally assemble the server.
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-  std::cout << "Server listening on " << server_address << std::endl;
+    ServerBuilder builder;
+    // Listen on the given address without any authentication mechanism.
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    // Register "service" as the instance through which we'll communicate with
+    // clients. In this case it corresponds to an *synchronous* service.
+    builder.RegisterService(&service);
+    // Finally assemble the server.
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
 
-  // Wait for the server to shutdown. Note that some other thread must be
-  // responsible for shutting down the server for this call to ever return.
-  server->Wait();
+    // Wait for the server to shutdown. Note that some other thread must be
+    // responsible for shutting down the server for this call to ever return.
+    server->Wait();
 }
 
-int main(int argc, char** argv) {
-  RunServer();
+int main(int argc, char **argv) {
+    RunServer();
 
-  return 0;
+    return 0;
 }
